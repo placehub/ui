c210e6b248427bfab50776e1b986cce4fc6e4aea
Denis <zabavnikov.com@gmail.com>
2022-11-22 19:24:40 +0700

    test

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default {
-  install: (app: any, options: object) => {
+  install: (app, options) => {
     app.config.globalProperties.$axios = axios.create({
       baseURL: options?.baseUrl || ''
     })
