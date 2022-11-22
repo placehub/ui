4eb2a062c688383451066ba506996d56789e527c
Denis <zabavnikov.com@gmail.com>
2022-11-22 19:34:27 +0700

    test

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,10 @@ import axios from 'axios'
 
 export default {
   install: (app, options) => {
-    app.config.globalProperties.$axios = axios.create({
-      baseURL: options?.baseUrl || ''
-    })
+    if (process.client) {
+      app.config.globalProperties.$axios = axios.create({
+        baseURL: options?.baseUrl || ''
+      })
+    }
   }
 }
