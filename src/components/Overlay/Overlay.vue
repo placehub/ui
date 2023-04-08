f7d6bb1034be31578ac8a62c75f296fdd3d937f1
Denis <zabavnikov.com@gmail.com>
2023-04-08 15:48:28 +0700

    wip

--- src/components/Overlay/Overlay.vue
+++ src/components/Overlay/Overlay.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="count" class="overlay" tabindex="1">
+  <div v-if="count" class="flex justify-center fixed top-0 right-[17px] bottom-0 left-0 overflow-hidden z-[9999]" tabindex="1">
     <component
       v-bind="overlay.props"
       v-for="(overlay, index) in overlay.stack"
@@ -12,7 +12,7 @@
         zIndex: index + 1
       }"
     ></component>
-    <div class="overlay-backdrop" :style="{zIndex: count - 1}" @click="overlay.hide"></div>
+    <div class="fixed top-0 right-0 bottom-0 left-0 z-[1] bg-black/50" :style="{zIndex: count - 1}" @click="overlay.hide"></div>
   </div>
 </template>
 
@@ -32,13 +32,3 @@ const onKeydown = (event) => {
 onMounted(() => document.addEventListener('keydown', onKeydown))
 onBeforeUnmount(() => document.removeEventListener('keydown', onKeydown))
 </script>
-
-<style>
-.overlay {
-  @apply flex justify-center fixed top-0 right-[17px] bottom-0 left-0 overflow-hidden z-[9999];
-}
-
-.overlay-backdrop {
-  @apply fixed top-0 right-0 bottom-0 left-0 z-[1] bg-black/50;
-}
-</style>
