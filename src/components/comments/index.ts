0ff4c4b13a98e6f4c6768bdc8d5a8ac33fbdfbe4
Denis <zabavnikov.com@gmail.com>
2023-04-21 09:02:34 +0700

    wip

--- src/components/comments/index.ts
+++ src/components/comments/index.ts
@@ -1,3 +1,3 @@
 export { default as BaseComment } from './components/BaseComment.vue'
 export { default as useComments } from './composables/useComments'
-export { useCommentsStore } from './store'
+export { default as useCommentsStore } from './store'
