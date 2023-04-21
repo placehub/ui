e82cfd3da189e38011184aa453290e1719af77f4
Denis <zabavnikov.com@gmail.com>
2023-04-21 09:01:57 +0700

    wip

---
+++ src/components/comments/components/BaseComment.vue
@@ -0,0 +1,33 @@
+<template>
+  <article :id="`comment-${comment.id}`">
+    <slot :onEdit="onEdit" :onReply="onReply" :isReply="isReply" />
+  </article>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useCommentsStore } from '@placehub/ui'
+
+const props = defineProps({
+  comment: {
+    type: Object,
+    required: true,
+  },
+})
+
+const store = useCommentsStore()
+
+const isReply = computed(() => parseInt(store.selectedComment?.id) === parseInt(props.comment.id))
+
+const onEdit = () => {
+  store.setSelectedComment(props.comment, 'edit')
+}
+
+const onReply = () => {
+  if (store.isReply || store.isEdit) {
+    store.setSelectedComment()
+  } else {
+    store.setSelectedComment(props.comment, 'reply')
+  }
+}
+</script>
