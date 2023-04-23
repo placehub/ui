b7ad1d1a8e8fe16fd492074eaa00d82a6cd1a487
Denis <zabavnikov.com@gmail.com>
2023-04-23 18:02:25 +0700

    wip

--- src/components/_common/ImageGrid/ImageGrid.vue
+++ src/components/_common/ImageGrid/ImageGrid.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="gallery" :class="classes[images.length]">
-    <div v-for="image in images" :key="image.id">
-      <img :src="image.url" alt="" :data-src="image.url">
+    <div v-for="(image, index) in images" :key="image.id">
+      <NuxtImg :src="image.url" alt="" :data-src="image.url" :width="size[index].width" :height="size[index].height" />
     </div>
   </div>
 </template>
@@ -9,7 +9,7 @@
 <script setup>
 import './styles.scss'
 
-defineProps({
+const props = defineProps({
   images: {
     type: Array,
     required: true,
@@ -27,4 +27,58 @@ const classes = {
   9:  'gallery-grid-9',
   10: 'gallery-grid-10'
 }
+
+const sizes = {
+  1: [{width: 480, height: 320}],
+  2: [{width: 480, height: 320}, {width: 160, height: 160}],
+  3: [{width: 320, height: 320}, {width: 200, height: 200}, {width: 200, height: 200}],
+  4: [{width: 480, height: 320}, {width: 200, height: 200}, {width: 200, height: 200}, {width: 200, height: 200}],
+  5: [
+    {width: 240, height: 240},
+    {width: 240, height: 240},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+  ],
+  6: [
+    {width: 480, height: 320},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+  ],
+  7: [
+    {width: 480, height: 320},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+  ],
+  8: [
+    {width: 480, height: 320},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 480, height: 320},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+  ],
+  9: [
+    {width: 480, height: 320},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+    {width: 160, height: 160},
+  ],
+}
+
+const size = sizes[props.images.length]
 </script>
