a85ac69198ee11a5f9e55741b19ff46e135cdc96
Denis <zabavnikov.com@gmail.com>
2023-05-05 09:40:39 +0700

    Доработка Overlay

--- src/components/_common/Overlay/Overlay.vue
+++ src/components/_common/Overlay/Overlay.vue
@@ -1,33 +1,42 @@
 <template>
-  <div :class="{[count ? 'flex justify-center fixed top-0 right-0 bottom-0 left-0 overflow-hidden z-[9999]' : '']: true}" tabindex="1">
-    <TransitionGroup
-      enter-active-class="transition duration-200 ease-out"
-      enter-from-class="translate-y-1 opacity-0"
-      enter-to-class="translate-y-0 opacity-100"
+  <Transition
+      enter-active-class="transition duration-100 ease-out"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
       leave-active-class="transition duration-150 ease-in"
-      leave-from-class="translate-y-0 opacity-100"
-      leave-to-class="translate-y-1 opacity-0"
-    >
-    <Component
-      v-bind="overlay.props"
-      v-for="(overlay, index) in overlay.stack"
-      v-on="overlay.on"
-      class="absolute"
-      role="dialog"
-      :is="overlay.component"
-      :key="overlay.key"
-      :style="{
-        zIndex: index + 1
-      }"
-    ></Component>
-    </TransitionGroup>
-    <div v-show="count" class="fixed top-0 right-0 bottom-0 left-0 z-[1] bg-black/50" :style="{zIndex: count - 1}" @click="overlay.hide"></div>
-  </div>
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+  >
+    <div v-show="count" class="grid grid-cols-12 fixed top-0 right-0 bottom-0 left-0 overflow-hidden z-[9999]"
+         tabindex="1">
+      <TransitionGroup
+          enter-active-class="transition duration-100 ease-out"
+          enter-from-class="translate-y-2 opacity-0"
+          enter-to-class="translate-y-0 opacity-100"
+          leave-active-class="transition duration-150 ease-in"
+          leave-from-class="translate-y-0 opacity-100"
+          leave-to-class="translate-y-2 opacity-0"
+      >
+        <Component
+          v-bind="overlay.props"
+          v-for="(overlay, index) in overlay.stack"
+          v-on="overlay.on"
+          role="dialog"
+          class="absolute"
+          :is="overlay.component"
+          :key="overlay.key"
+          :style="{ zIndex: index + 1 }">
+        </Component>
+      </TransitionGroup>
+      <div class="fixed top-0 right-0 bottom-0 left-0 z-[1] bg-black/50" :style="{zIndex: count - 1}"
+           @click="overlay.hide"></div>
+    </div>
+  </Transition>
 </template>
 
 <script setup>
 import useOverlay from './useOverlay.ts'
-import { computed, onMounted, onBeforeUnmount, watch } from 'vue'
+import { computed, onBeforeUnmount, onMounted, watch } from 'vue'
 
 const overlay = useOverlay()
 const count = computed(() => overlay.stack.length)
